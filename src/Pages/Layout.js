be88aa5fecb577f4fd0f90682a10fae7d02814e8
nguyenhuy22711 <nguyenhuy22711@gmail.com>
2023-11-09 16:50:26 +0700

    Update Layout

--- src/Pages/Layout.js
+++ src/Pages/Layout.js
@@ -17,7 +17,7 @@ const Layout = () => {
       <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container-fluid">
           <a class="navbar-brand" >
-            Navbar
+            BookStore
           </a>
           <button
             class="navbar-toggler"
@@ -124,13 +124,13 @@ const Layout = () => {
   <div class="container my-5">
 
     <footer
-            class="text-center text-lg-start text-white footer1"
+            class="text-center text-lg-start text-white footer-background"
             >
       <section
               class="d-flex justify-content-between p-4"
               >
         <div class="me-5">
-          <span>Get connected with us on social networks:</span>
+          <span>Liên hệ với chúng tôi:</span>
         </div>
 
         <div>
@@ -159,21 +159,19 @@ const Layout = () => {
         <div class="container text-center text-md-start mt-5">
           <div class="row mt-3">
             <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
-              <h6 class="text-uppercase fw-bold">Company name</h6>
+              <h6 class="text-uppercase fw-bold">Cửa hàng Bán Sách</h6>
               <hr
-                  class="mb-4 mt-0 d-inline-block mx-auto footer3"
+                  class="mb-4 mt-0 d-inline-block mx-auto footer-shop"
                   />
               <p>
-                Here you can use rows and columns to organize your footer
-                content. Lorem ipsum dolor sit amet, consectetur adipisicing
-                elit.
+                Tại đây có bán tất cả loại sách , đa dạng ,phong phú
               </p>
             </div>
 
             <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
               <h6 class="text-uppercase fw-bold">Products</h6>
               <hr
-                  class="mb-4 mt-0 d-inline-block mx-auto footer4"
+                  class="mb-4 mt-0 d-inline-block mx-auto footer-product"
                   />
               <p>
                 <a href="#!" class="text-white">MDBootstrap</a>
@@ -192,7 +190,7 @@ const Layout = () => {
             <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
               <h6 class="text-uppercase fw-bold">Useful links</h6>
               <hr
-                  class="mb-4 mt-0 d-inline-block mx-auto footer5"
+                  class="mb-4 mt-0 d-inline-block mx-auto footer-usefull"
                   />
               <p>
                 <a href="#!" class="text-white">Your Account</a>
@@ -211,23 +209,23 @@ const Layout = () => {
             <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
               <h6 class="text-uppercase fw-bold">Contact</h6>
               <hr
-                  class="mb-4 mt-0 d-inline-block mx-auto footer6"
+                  class="mb-4 mt-0 d-inline-block mx-auto footer-contact"
                   />
-              <p><i class="fas fa-home mr-3"></i> New York, NY 10012, US</p>
-              <p><i class="fas fa-envelope mr-3"></i> info@example.com</p>
-              <p><i class="fas fa-phone mr-3"></i> + 01 234 567 88</p>
-              <p><i class="fas fa-print mr-3"></i> + 01 234 567 89</p>
+              <p><i class="fas fa-home mr-3"></i> 65 Đ. Huỳnh Thúc Kháng, Bến Nghé, Quận 1, Thành phố Hồ Chí Minh 700000, Việt Nam  </p>
+              <p><i class="fas fa-envelope mr-3"></i> bookstore@gmail.com</p>
+              <p><i class="fas fa-phone mr-3"></i> 0817287365</p>
+              <p><i class="fas fa-print mr-3"></i> 0817287365</p>
             </div>
           </div>
         </div>
       </section>
 
       <div
-          class="text-center p-3 footer7"
+          class="text-center p-3 footer-end"
           >
-        © 2020 Copyright:
+        © 2023 Copyright:
         <a class="text-white" href="https://mdbootstrap.com/"
-          >MDBootstrap.com</a
+          > BookStore.com</a
           >
       </div>
     </footer>
